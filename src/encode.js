2b1858e785b20d90e5714ed423066de511e49c6b
Andrea <andry.tino@gmail.com>
2025-03-26 23:21:10 +0100

    Add recursive implementation

--- src/encode.js
+++ src/encode.js
@@ -17,4 +17,29 @@ function encode(input) {
     return output;
 }
 
-module.exports = { encode };
+/**
+ * Encodes a string.
+ * @param {string} input The string to encode.
+ * @param {string} output The recursive carry on output. 
+ * @returns {string} The encoded string.
+ */
+function encode_recursive(input, output) {
+    if (!input) {
+        // Final condition
+        return output || '';
+    }
+
+    if (!output) {
+        // Initial condition
+        return encode_recursive(input, `${input[0]}0`);
+    }
+
+    const currentChar = output[output.length-2];
+    const length = parseInt(output[output.length-1]);
+    if (input[0] === currentChar) {
+        return encode_recursive(input.substring(1), `${output.substring(0, output.length-1)}${length+1}`);
+    }
+    return encode_recursive(input.substring(1), `${output}${input[0]}${1}`);
+}
+
+module.exports = { encode, encode_recursive };
--- src/encode.test.js
+++ src/encode.test.js
@@ -1,27 +1,38 @@
-const { encode } = require('./encode');
+const { encode, encode_recursive } = require('./encode');
 
-describe('encode', () => {
+/**
+ * @param {(input: string) => string} encodeFunc 
+ */
+const allTests = (encodeFunc) => {
     it('should return an empty string when input is an empty string', () => {
-        expect(encode("")).toBe("");
+        expect(encodeFunc("")).toBe("");
     });
 
     it('should encode a string with no repeated characters', () => {
-        expect(encode("abc")).toBe("a1b1c1");
+        expect(encodeFunc("abc")).toBe("a1b1c1");
     });
 
     it('should encode a string with repeated characters', () => {
-        expect(encode("aaabbc")).toBe("a3b2c1");
+        expect(encodeFunc("aaabbc")).toBe("a3b2c1");
     });
 
     it('should handle strings with a single character', () => {
-        expect(encode("a")).toBe("a1");
+        expect(encodeFunc("a")).toBe("a1");
     });
 
     it('should handle strings with all identical characters', () => {
-        expect(encode("aaaa")).toBe("a4");
+        expect(encodeFunc("aaaa")).toBe("a4");
     });
 
     it('should handle strings with mixed case characters', () => {
-        expect(encode("aAaa")).toBe("a1A1a2");
+        expect(encodeFunc("aAaa")).toBe("a1A1a2");
     });
+};
+
+describe('encode', () => {
+    allTests(encode);
+});
+
+describe('encode recursive', () => {
+    allTests(encode_recursive);
 });
