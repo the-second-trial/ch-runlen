7d309b37d83b4d2516f96eaa3c9c8c461c2628e2
Andrea <andry.tino@gmail.com>
2025-03-27 09:01:17 +0100

    Improve program

--- src/index.js
+++ src/index.js
@@ -1,3 +1,10 @@
 const { encode } = require("./encode");
 
-encode("swssw");
+const args = process.argv.slice(2);
+if (args.length === 0) {
+    console.error('An input string must be provided');
+    process.exit(1);
+}
+
+const decoded = encode(args[0]);
+console.log('Decoded:', decoded);
